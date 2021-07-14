e06360d098a1b95c94f22db308462f964c39bf0c
Emp3reur <87422205+Emp3reur@users.noreply.github.com>
2021-07-14 13:04:03 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+Client.on("ready", () =>{
+    console.log("Nice frérot");
+});
+
+Client.login("ODY0NTg4ODA5OTI3OTE3NTc4.YO3pEw.6O5MvjnGgeiNqaJOQtEsmV92OkY")
